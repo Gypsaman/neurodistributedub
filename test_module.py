8149ac0dc9874e70720f41c25928885908fa7ecd
Gypsaman <gypsaman@gmail.com>
2023-02-14 16:14:02 -0600

    added quiz import

--- test_module.py
+++ test_module.py
@@ -3,8 +3,13 @@ from webproject.modules.table_creator import TableCreator,Field, only_contract, 
 from webproject import db, create_app
 from webproject.modules.ubemail import UBEmail
 from webproject.models import Submissions, User
+from webproject.modules import offline_utils, roster
+from datetime import datetime as dt
 
-check_submissions()
+offline_utils.import_quiz('Mid Term Quiz',dt.strptime('02/13/2023 20:30', "%m/%d/%Y %H:%M"))
+
+# offline_utils.grade_update('SP23-Monday')
+# check_submissions()
 # with create_app().app_context():
 #     user = User.query.filter_by(student_id='1164271').first()
 #     submissions = Submissions.query.filter_by(id=778).all()
--- webproject/modules/offline_utils.py
+++ webproject/modules/offline_utils.py
@@ -3,6 +3,30 @@ import json
 import requests
 from webproject.modules.ubemail import UBEmail
 from webproject.modules import roster
+from webproject import db, create_app
+from webproject.models import User, Assignments, Submissions, Grades, Sections, DueDates
+from datetime import datetime as dt
+
+
+Assignment_qry = 'SELECT * from Assignments inner join (SELECT assignment,duedate FROM due_dates WHERE section = {}) as d on Assignments.id = d.assignment'
+
+def grade_update(section_name=None):
+    with create_app().app_context():
+        if section_name is None:
+            users = User.query.filter_by(role='student').all()
+        else:
+            section = Sections.query.filter_by(section=section_name).first()
+            users = User.query.filter_by(section=section.id,role='student').all()
+        assignments = db.session.execute(Assignment_qry.format(section.id)).all()
+        
+        for user in users:
+            body = f'Hello {user.first_name} {user.last_name},\n\nYour grades are:\n'
+            for assignment in assignments:
+                grade = Grades.query.filter_by(user_id=user.id,assignment=assignment.id).first()
+                body += f'\t{assignment.name} - {grade.grade}\n' if grade is not None else f'\t{assignment.name} - Not submitted\n'
+            print(body)
+            break
+                    
 
 def grade_history():
         
@@ -45,5 +69,32 @@ def Assignments_not_completed():
         email = UBEmail()
         email.send_email(students[student_id]['Preferred Email'],'Assignments not completed',body)
         
+def import_quiz(quiz_name,quizdate):
+    with create_app().app_context():
+        assignment = Assignments.query.filter_by(name=quiz_name).first()
+        if assignment is None:
+            print(f'Could not find assignment {quiz_name}')
+            return
+        lookup = {info['Canvas ID']:student for student,info in roster.open_roster_encrypted().items()}
+        grades = open('./data/quizimport.csv','r').readlines()
+        for grade in grades[1:]:
+            student_id,grade = grade.split(',')
+            student_id = student_id.strip()
+            grade = grade.strip().replace('/n','')
+            if student_id not in lookup:
+                print(f'Could not find student {student_id}')
+                continue
+            student = lookup[student_id]
+            user = User.query.filter_by(student_id=student).first()
+            if user is None:
+                print(f'Could not find user {student}')
+                continue
+            if Grades.query.filter_by(user_id=user.id,assignment=assignment.id).first() is not None:
+                print(f'Grade already exists for {student}')
+                continue
+            grade = Grades(user_id=user.id,assignment=assignment.id,grade=grade,dategraded=quizdate)
+            db.session.add(grade)
+            db.session.commit() 
+    
 if __name__ == '__main__':
-    grade_history()
+    grade_update('SP23-Monday')
