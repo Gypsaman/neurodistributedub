6963ddf797537461e5b0f1bd523f0f4ddb159e4e
Gypsaman <gypsaman@gmail.com>
2023-08-12 12:00:48 -0500

    Initial Data and Quiz Creation

--- dbutilities/create_initial_data.py
+++ dbutilities/create_initial_data.py
@@ -6,24 +6,34 @@ from werkzeug.security import generate_password_hash
 def create_initial_data():
 
     with create_app().app_context():
-        section = Sections(
-            section='Fall 2023',
-            active = True
+        
+        create_sections()
+        create_user()
+        
+        
+def create_quizzes():
+    pass
+
+def create_sections():
+    section = Sections(
+        section='FA23-Monday',
+        active = True
+    )
+    db.session.add(section)
+    db.session.commit()
+def create_user():
+    section = Sections.query.filter_by(section='FA23-Monday').first() 
+    user = User(
+        email='gypsaman@gmail.com',
+        password=generate_password_hash('123',method='sha256'),
+        first_name='Cesar',
+        last_name ='Garcia',
+        student_id='553029',
+        role='admin',
+        section=section.id
         )
-        db.session.add(section)
-        db.session.commit()
-        section = Sections.query.filter_by(section='Fall 2023').first() 
-        user = User(
-            email='gypsaman@gmail.com',
-            password=generate_password_hash('b123',method='sha256'),
-            first_name='Cesar',
-            last_name ='Garcia',
-            student_id='553029',
-            role='admin',
-            section=section.id
-            )
-        db.session.add(user)
-        db.session.commit()
+    db.session.add(user)
+    db.session.commit()
         
 if __name__ == '__main__':
     create_initial_data()
--- webproject/modules/quizzes.py
+++ webproject/modules/quizzes.py
@@ -2,7 +2,7 @@ import json
 from collections import Counter
 import numpy as np
 from webproject import create_app, db
-from webproject.models import Quizzes, Questions, Answers, User, Sections
+from webproject.models import Quizzes, Questions, Answers, User, Sections, Quiz_Header, Quiz_Topics
 from datetime import datetime as dt
 from datetime import timedelta
 
@@ -22,18 +22,41 @@ def select_topics_final():
         topic_selected['Brownie'] -= 1
     return topic_selected
 
-def create_quiz(topics_selected,
-                date_available,
-                date_due,
-                description,
-                user_id,
-                multiple_retries=True
+def create_quiz(
+    description: str,
+    date_available: dt.datetime,
+    date_due: dt.datetime,
+    topics: dict,
+    multiple_retries: bool=True,
+    active: bool=True
     ):
-    quiz = Quizzes(description=description,user_id=user_id, date_available=date_available,date_due=date_due,submitted=False,multiple_retries=multiple_retries)
+    quiz_header = Quiz_Header(description=description,
+                              date_available=date_available,
+                              date_due=date_due,
+                              multiple_retries=multiple_retries,
+                              active=active)
+    db.session.add(quiz_header)
+    db.session.commit()
+    for topic,qty in topics.items():
+        quiz_topic = Quiz_Topics(quiz_id=quiz_header.id,
+                                 topic=topic,
+                                 number_of_questions=qty)
+        
+
+def create_quiz_user(quiz_id: int,
+                user_id,
+                
+    ):
+    quiz = Quizzes(quiz_header=quiz_id,
+                   user_id=user_id, 
+                   submitted=False,
+                   grade=None)
     db.session.add(quiz)
     db.session.commit()
     question_number = 1
-    for topic,qty in topics_selected.items():
+    for quiz_topic in Quiz_Topics.query.filter_by(quiz_header=quiz_id).all():
+        topic = quiz_topic.topic
+        qty = quiz_topic.number_of_questions
         selection = np.random.choice(range(Topics[topic]),qty ,replace=False)
         topic_questions = np.array([id for id,q in questions.items() if q['Topic'] == topic])
         for q in topic_questions[selection]:
@@ -60,18 +83,25 @@ def create_final(topics_selected:dict,StartTime:dt):
            
 
 def create_quiz_all_users(section_name:str,
-                          description,
-                          topics_selected:dict
+                          quiz_id:int,
     ):     
     with create_app().app_context():
         section = Sections.query.filter_by(section=section_name).first()
         users = User.query.filter_by(section=section.id,role='student').all()
         for user in users:
-            quiz_id = create_quiz(topics_selected,dt.now(),dt.now()+timedelta(days=7),description=description + ' for {}'.format(user.first_name),user_id=user.id)
+            quiz_id = create_quiz_user(quiz_id,user.id)
             print('Created quiz {} for {}'.format(quiz_id,user.first_name))           
 
     
 if __name__ == '__main__':
     topics_selected = {"NFT": 11}
-    create_quiz_all_users('SP23-Wednesday','NFT',topics_selected)
+    quiz_id = create_quiz(
+                description= "NFT Quiz",
+                date_available= dt.now(),
+                date_due= dt.now()+timedelta(days=7),
+                topics = topics_selected,
+                multiple_retries=True,
+                active=True
+                )
+    create_quiz_all_users('SP23-Wednesday',quiz_id)
 
