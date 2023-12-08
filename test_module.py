c07e253aac23d7c6d4b404c9615a7d6091076241
Gypsaman <gypsaman@gmail.com>
2023-12-08 08:29:42 -0600

    final quiz

--- test_module.py
+++ test_module.py
@@ -4,6 +4,10 @@ import json
 from webproject import create_app, db
 from webproject.modules.offline_utils import email_course_evaluation
 from graders.check_submissions import check_submissions
+from webproject.models import User,Quiz_Header, Quizzes
 
+with create_app().app_context():
+    qh = Quiz_Header.query.filter_by(description='Final').first()
+    quiz = Quizzes.query.filter_by(quiz_header=qh.id,user_id=127).first()
+    print(quiz)
 
-check_submissions()
