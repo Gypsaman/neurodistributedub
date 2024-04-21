8cb29e61e84ddae0377ab774e032460ff8116e68
gypsaman <gypsaman@gmail.com>
2024-04-21 16:31:23 -0500

    logger

--- webproject/modules/logger.py
+++ webproject/modules/logger.py
@@ -11,8 +11,9 @@ class LogType(Enum):
     
     
 def Log(log_type, user, message):
-    with open(".\logs\log.txt", "a") as f:
-        f.write(f"{log_type.name},{dt.now().strftime('%Y-%m-%d %H:%M')},{user},{message}\n")
+    return
+    # with open(".\logs\log.txt", "a") as f:
+    #     f.write(f"{log_type.name},{dt.now().strftime('%Y-%m-%d %H:%M')},{user},{message}\n")
 
 if __name__ == "__main__":
     Log(LogType.LOGIN, "admin", "admin logged in")
