8813901c5e95df05675b5a30dc35b9603c11db80
gypsaman <gypsaman@gmail.com>
2024-07-10 16:11:15 -0500

    add wsgi file

---
+++ dna_wsgi.py
@@ -0,0 +1,19 @@
+```{python}
+import os
+import time
+import site
+os.environ["TZ"] = "America/New_York"
+time.tzset()
+
+import sys
+#
+path = '/var/www/neurodistributedub'
+if path not in sys.path:
+    sys.path.append(path)
+path = '/var/www/neurodistributedub/venv/lib/python3.9/site-packages'
+if path not in sys.path:
+    sys.path.append(path)
+
+print(sys.path)
+from webproject import create_app   # noqa
+application = create_app()
