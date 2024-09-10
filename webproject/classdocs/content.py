83838a268ba53533009556f91aff12eea7d67979
Cesar Garcia <gypsaman@gmail.com>
2024-09-10 08:15:56 -0400

    Update content.py with all classes

--- webproject/classdocs/content.py
+++ webproject/classdocs/content.py
@@ -23,4 +23,40 @@ content = {
         "videos":[]
 
     }
+    "Smart contracts":{
+        "slides":[],
+        "videos":[]
+    },
+    "Solidity Programming & Oracles":{
+        "slides":[],
+        "videos":[]
+    },
+    "Contract Factory Oracle practice":{
+        "slides":[],
+        "videos":[]
+    },
+    "Web3.py":{
+        "slides":[],
+        "videos":[]
+    },
+    "Foundry":{
+        "slides":[],
+        "videos":[]
+    },
+    "Mocks and Testing":{
+        "slides":[],
+        "videos":[]
+    },
+    "IPFS and Tokens":{
+        "slides":[],
+        "videos":[]
+    },
+    "NFT":{
+        "slides":[],
+        "videos":[]
+    },
+    "NFT – Contract Interaction":{
+        "slides":[],
+        "videos":[]
+    },
 }
